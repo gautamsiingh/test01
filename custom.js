70569f02c75f09ab9d0b4cbc246704a3ef4e7a1e
Gautam <gautamgambhir639@gmail.com>
2021-02-17 15:13:06 +0530

    init

--- custom.js
+++ custom.js
@@ -58,7 +58,7 @@ item.onkeyup = function(event) {
 var verses = [
     { 
         title: "Genesis", 
-        verse: "In the beginning God created the heaven and the earth. 1:2 And the earth was without form, and void; and darkness was upon the face of the deep. And the Spirit of God moved upon the face of the waters."
+        verse: "In the beginning God created the heaven and the earth 1:2 And the earth was without form, and void; and darkness was upon the face of the deep. And the Spirit of God moved upon the face of the waters."
     },
     { 
         title: "Genesis 2", 
